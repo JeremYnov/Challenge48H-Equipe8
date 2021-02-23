1500a42934ca243c64f269c32f8e1e5be1a915c2
JeremYnov <jeremy.richard@ynov.com>
2021-02-23 12:27:53 +0100

    last git push

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 import os
 import pymongo
 
+import ast
+
 from config import DevelopmentConfig
 from werkzeug.utils import secure_filename
 
@@ -30,9 +32,10 @@ def homepage():
         current_dir = os.getcwd()
         current_path = current_dir + "\\pictures\\"
         print(request.values.get('picture_type'))
-        return render_template("index.html")
+        return render_template("pages/index.html")
     else:
-        return render_template("index.html")
+        images = [ a for a in col.find() ]
+        return render_template("pages/index.html", images=images)
 
 
 @app.route("/update/<path>", methods=["GET", "POST"])
@@ -40,7 +43,7 @@ def update(path):
     if request.method == 'GET':
         obj = col.find_one({"path": path})
         if obj:
-            return render_template("update.html", path=path, obj=obj)
+            return render_template("pages/update.html", path=path, obj=obj)
         elif not obj:
             return redirect(url_for('create'))
         print('ERROR')
@@ -52,7 +55,7 @@ def create():
     if req:
         picture = request.files["picture"]
         current_dir = os.getcwd()
-        path = current_dir + "\\app\\static\\"
+        path = current_dir + "\\static\\"
         imgPath = path + secure_filename(picture.filename)
         picture.save(imgPath)
         pictureName = req.get("picture_name")
@@ -87,25 +90,37 @@ def create():
         else:
             pictureLimited = True
         # pictureLimited = req.get("is_limited")
-
+        i = 0
+        test = 0
+        tags = []
+        if req.get("tag0") : 
+            while test == 0:
+                tag = "tag" + str(i)
+                if req.get(tag) is not None :
+                    tags.append(req.get(tag))
+                else :
+                    test = 1
+                i += 1
+        print(tags)
+            
+        
         pictureCredit = req.get("picture_author")
         pictureCopyright = req.get("picture_copyright")
         pictureDate = req.get("picture_date")
-        pictureTags = []
         insert = {"name": pictureName, "type": pictureType, "path": secure_filename(picture.filename), "with_product": pictureWithProduct,
                   "with_human": pictureWithHuman, "institutional": pictureInstitutional, "format": pictureVertical,
                   "credits": pictureCredit, "user_permissions": pictureLimited, "copyright": pictureCopyright,
-                  "end_date": pictureDate, "tags": pictureTags}
+                  "end_date": pictureDate, "tags": tags}
         col.insert_one(insert)
         print(picture)
-    return render_template("create.html")
+    return render_template("pages/create.html")
 
 
 @app.route("/delete/<path>", methods=["GET", "POST"])
 def delete(path):
     col.delete_one({"path": path})
     message = 'Image has been deleted !'
-    return render_template("index.html", message=message)
+    return render_template("pages/index.html", message=message)
 
 
 @app.route("/upload", methods=['GET', 'POST'])
@@ -122,60 +137,73 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('homepage'))
-    return render_template("upload.html")
+    return render_template("pages/upload.html")
 
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    response = request.json
-    search = []
 
-    filtersDic = {
-        'name':                '{ "name" : {"$regex": \'.*{}.*\' }}',
-        'type':                '{ "type" : {"$regex": \'.*{}.*\' }}',
-        'credits':             '{ "credits" : {"$regex": \'.*{}.*\' }}',
-        'with_product':        '{ "with_product" : {"$eq" :  {} }}',
-        'with_humans':         '{ "with_humans" :  {"$eq" :  {} }}',
-        'institutional':       '{ "institutional" : {"$eq" :  {} }}',
-        'format':              '{ "format" : {"$eq" :   {} }}',
-        'tags':                '{ "tags" :         {"$in" :  {} }}',
-    }
+    req = request.form
 
-    if 'filters' in response:
-        for key, value in response['filters'].items():
+    if req:
+        
+        filters = {
+            'with_product' : 'true' if req.get("with_product") == 'on' else 'false',
+            'with_human' : 'true' if req.get("with_human") == 'on' else 'false',
+            'institutional' : 'true' if req.get("institutional") == 'on' else 'false',
+            'format' : 'true' if req.get("is_vertical") == 'on' else 'false'
+        }
+
+        if req.get("name") != '':
+            filters['name'] = req.get("name") 
+
+        if req.get("type") != '':
+            filters['type'] = req.get("type") 
+
+        if req.get("credits") != '':
+            filters['credits'] = req.get("credits") 
+
+        if req.get("tags") != '':
+            filters['tags'] = req.get("tags") 
+            print(req.get("tags"))
+            print(type(req.get("tags")))
+        
+        print(filters)
+
+        filtersDic = {
+                'name':                '{ "name" : {"$regex": \'.*{}.*\' }}',
+                'type':                '{ "type" : {"$regex": \'.*{}.*\' }}',
+                'credits':             '{ "credits" : {"$regex": \'.*{}.*\' }}',
+                'tags': '{ "tags" : {"$in" : {} }}',
+                'with_product':        '{ "with_product" : {"$eq" :  {} }}',
+                'with_human':         '{ "with_humans" :  {"$eq" :  {} }}',
+                'institutional':       '{ "institutional" : {"$eq" :  {} }}',
+                'format':              '{ "format" : {"$eq" :   {} }}',
+            }
+
+        search = []
+        for key, value in filters.items():
             if key in filtersDic:
-                newFilter = filtersDic[key].replace('{}', str(value))
-                search.append(ast.literal_eval(newFilter))
-            else:
-                search = []
+                newFilter = filtersDic[key].replace ( '{}', value )
+                print(newFilter)
+                print(type(newFilter))
+                search.append( ast.literal_eval( str(newFilter) ))
 
-    if len(search):
-        response = [a for a in col.find({"$and": search}, {"_id": 1})]
-
-    else:
-        response = {"output": {
-            "type": "notify",
-            "description": "no entities found"
-        }}
-
-        if 'filters' in response:
-            for key, value in response['filters'].items():
-                if key in filtersDic:
-                    newFilter = filtersDic[key].replace('{}', str(value))
-                    search.append(ast.literal_eval(newFilter))
-                else:
-                    search = []
+        print(search)
 
         if len(search):
-            response = [a for a in col.find({"$and": search}, {"_id": 1})]
-
-            return jsonify(response)
+            images = [ a for a in col.find({ "$and" : search }) ]
+            
 
         else:
-            return jsonify("hello")
+            images = [ a for a in col.find() ]
 
-    if request.method == 'GET':
-        return render_template("search.html")
+        print(images)
+
+        return render_template("pages/search.html", images=images)
+    
+    else:
+        return render_template("pages/search.html")
 
 
 if __name__ == "__main__":
